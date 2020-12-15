5615acd5d5f5e91f9448426a6721a41be2c1c54f
seraph <seraphinush@gmail.com>
2020-12-15 19:12:31 +0900

    1.00.08.v101.02

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class auto_daily_credit {
 
   constructor(mod) {
 
+    // code
     mod.hookOnce('S_LOGIN', 'event', () => {
       if (!mod.trySend('C_REQUEST_RECV_DAILY_TOKEN', 1, {})) {
         mod.log('Unmapped protocol packet \<C_REQUEST_RECV_DAILY_TOKEN\>.');
